83318398a4785dd968b0a605d3e4a2328908d5c1
Sergei <vlasovseverin@gmail.com>
2024-11-23 17:51:59 +0100

    Домашняя работа 24

--- src/main/kotlin/org/kotlincourse/lesson24/Homework/MyCustomException.kt
+++ src/main/kotlin/org/kotlincourse/lesson24/Homework/MyCustomException.kt
@@ -1,3 +1,6 @@
 package org.sergei.org.kotlincourse.lesson24.Homework
 
+//Задание 4. Создай свой тип исключения в отдельном файле, наследуемый от AssertionError
+// и принимающий текст. Выброси это исключение в main
+
 class MyCustomException(message: String) : AssertionError(message)
--- src/main/kotlin/org/kotlincourse/lesson24/Homework/MyCustomRuntimeException.kt
+++ src/main/kotlin/org/kotlincourse/lesson24/Homework/MyCustomRuntimeException.kt
@@ -1,3 +1,9 @@
 package org.sergei.org.kotlincourse.lesson24.Homework
 
+// Задание 5. Создай свой тип исключения в отдельном файле,
+// наследуемый от RuntimeException и принимающий ArrayIndexOutOfBoundsException
+// тип в качестве аргумента. Напиши код, который спровоцирует выброс
+// ArrayIndexOutOfBoundsException, перехвати его с помощью try-catch
+// и в блоке catch выведи сообщение в консоль и сделай выброс своего типа исключения.
+
 class MyCustomRuntimeException(cause: ArrayIndexOutOfBoundsException) : RuntimeException(cause)
